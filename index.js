4500bfd0e6aea36318d7525b58b24d0279088a69
Bavara Kumar Swami <kumarbevara7708@gmail.com>
2023-07-05 22:24:43 +0530

    add api code

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/order");
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
+app.use(express.json());
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("DB connection sucessfull");
+}).catch((err) => {
+    console.log(err);
+});
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+
+
+app.listen(process.env.PORT || 5000, (req, res) => {
+    console.log("server started at 5000");
+})
